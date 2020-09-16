e80631e4c18dd8e2726c5ef3f753857d30b398ec
haileyhansard <haileyhansard@gmail.com>
2020-09-16 10:00:49 -0700

    Tests working for searching and sorting files, with comments from lecture added

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,53 @@
 # TO-DO: Implement a recursive implementation of binary search
+# arr: the input array to search through
+# target: the target we're searching for
+# start: left-side index of the sub-array we're searching through
+# end: right-side index of the sub-array we're searching through
 def binary_search(arr, target, start, end):
     # Your code here
+    #check base case
+    #make sure to call the function in itself
+    #each recursive call should bring us closer to reaching the base case(s)
+    #it stops when we either find the target or start and end go past each other (i.e. we don't find the target)
+    
+    if start > end:  
+    #first base case
+    # if start becomes greater than end, we stop and the target isn't in the array
+        return -1
+    else:
+        mid = (start + end) // 2
 
+        if arr[mid] == target:  
+        #this is our second base case
+            return mid
+        
+        #otherwise, we need to move towards one of our base cases
+        #arr doesn't change, just because we'll end updating start and end
+        #target doesn't change between recursive calls
+        #end changes if we toss out the right side of the array
+        elif arr[mid] > target:
+            return binary_search(arr, target, start, mid - 1)
+
+        #start changes if we toss out out the left side of the array 
+        else:
+            return binary_search(arr, target, mid + 1, end)
+
+
+
+    # if end >= start:
+    #     mid = (end + start) // 2
+    #     #if element is present at the middle itself
+    #     if arr[mid] == target:
+    #         return mid       
+    #     #if element is smaller than mid, then it can only be present in the left subarray
+    #     elif arr[mid] > target:
+    #         return binary_search(arr, start, mid - 1, target)
+    #     #else, the element can only be present in right subarray
+    #     else:
+    #         return binary_search(arr, mid + 1, end, target)
+    # else:
+    #     #element is not present in the array
+    #     return -1
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
@@ -9,6 +55,7 @@ def binary_search(arr, target, start, end):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
-def agnostic_binary_search(arr, target):
-    # Your code here
 
+# def agnostic_binary_search(arr, target):
+#     # Your code here
+#     pass
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,17 +1,82 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
+    #merge function is used for merging two halves
     elements = len(arrA) + len(arrB)
+    #pre-allocating our output array with the number of elements we know it will hold at the end
     merged_arr = [0] * elements
 
     # Your code here
+    a = 0
+    b = 0
 
-
+    for i in range(elements):
+        #check if a is in bounds of arrA
+        if a >= len(arrA):
+            #we've moved all elements from arrA into merged_arr already
+            #we still have elements from arrB that need to be moved
+            merged_arr[i] = arrB[b]
+            b += 1
+        #check if b is in bounds of arrB
+        elif b >= len(arrB):
+            #we've moved all the elements from arrB into merged_arr
+            #we still have elements from arrA that need to be moved
+            merged_arr[i] = arrA[a]
+            a += 1
+        #compare arrA[a] against arrB[b]
+        elif arrA[a] < arrB[b]:
+            #move the smaller element into the merged_arr
+            merged_arr[i] = arrA[a]
+            a += 1
+        else:
+            merged_arr[i] = arrB[b]
+            b += 1
+    
     return merged_arr
+
+
+    # if arrA[a] <= arrB[b]:
+    #     output.append(arrA[a])
+    #     a += 1
+    # else:
+    #     output.append(arrB[b])
+    #     b += 1
+
+    #return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     # Your code here
+    if len(arr) > 1:
+        L = merge_sort(arr[0 : len(arr) // 2])
+        R = merge_sort(arr[len(arr) // 2:])
+        #mid = len(arr)//2 #finding the mid of the array by dividing it in half
+        #L = arr[:mid] #left is equal to the first index to the mid point. could also do arr[:mid]
+        #R = arr[mid:] #right is equal to the mid point through the rest of the array to end. could also do arr[mid:]
 
+        #merge_sort(L) #sort the first half
+        #merge_sort(R) #sort the second half
+        arr = merge(L, R)
+
+        # i = j = k = 0
+
+        # while i < len(L) and j <len(R):
+        #     if L[i] < R[j]:
+        #         arr[k] = L[i]
+        #         i += 1
+        #     else:
+        #         arr[k] = R[j]
+        #         j += 1
+        #     k += 1
+
+        # while i < len(L):
+        #     arr[k] = L[i]
+        #     i += 1
+        #     k += 1
+
+        # while j < len(R):
+        #     arr[k] = R[j]
+        #     j += 1
+        #     k += 1
 
     return arr
 
@@ -19,10 +84,11 @@ def merge_sort(arr):
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
 # or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
-    # Your code here
+
+# def merge_in_place(arr, start, mid, end):
+#     # Your code here
 
 
-def merge_sort_in_place(arr, l, r):
-    # Your code here
+# def merge_sort_in_place(arr, l, r):
+#     # Your code here
 
